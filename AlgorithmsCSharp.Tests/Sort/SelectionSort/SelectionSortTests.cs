353b42d053fcf39160f4ed1df6854dd71a49ee6c
Marat Abdrakhmanov <marat-ab@mail.ru>
2020-11-06 18:13:26 +0500

    [fix] alg sort for immutable, [add] selection sort mutable edition and tests

--- AlgorithmsCSharp.Tests/Sort/SelectionSort/SelectionSortTests.cs
+++ AlgorithmsCSharp.Tests/Sort/SelectionSort/SelectionSortTests.cs
@@ -18,7 +18,95 @@ namespace AlgorithmsCSharp.Tests
 
         [Test]
         public void SelectionSortImmutableEmptyTest() =>
-            Assert.AreEqual(SelectionSort.SelectionSortImmutable(new List<int>(), comparer), new List<int>());
+            Assert.AreEqual(SelectionSort.SelectionSortImmutableForValueTypes(new List<int>(), comparer), new List<int>());
+
+        [Test]
+        public void SelectionSortImmutableOneElementTest() =>
+            Assert.AreEqual(SelectionSort.SelectionSortImmutableForValueTypes(new List<int>() { 1 }, comparer), new List<int>() { 1 });
+
+        [Test]
+        public void SelectionSortImmutableSimpleArrayTest() =>
+            Assert.AreEqual(SelectionSort.SelectionSortImmutableForValueTypes(new List<int>() { 3, 2, 5, 1, 6, 8, 9, 4, 7 }, comparer),
+                new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+                );
+
+        [Test]
+        public void SelectionSortImmutableArrayWithDoublesTest() =>
+            Assert.AreEqual(SelectionSort.SelectionSortImmutableForValueTypes(new List<int>() { 3, 2, 3, 1, 4, 2, 1, 4 }, comparer),
+                new List<int>() { 1, 1, 2, 2, 3, 3, 4, 4 }
+                );
+
+        [Test]
+        public void SelectionSortImmutableSomeArraysTest()
+        {
+            for (int s = 1; s < 10; s++)
+            {
+                var arr = Enumerable.Range(1, s).ToList();
+                var tmpArr = arr.Select(item => item).ToList();
+                Shuffle(tmpArr);
+                Assert.AreEqual(SelectionSort.SelectionSortImmutableForValueTypes(tmpArr, comparer), arr);
+            }
+        }
+
+        [Test]
+        public void SelectionSortMutableEmptyTest()
+        {
+            var arr = new List<int>();
+            SelectionSort.SelectionSortMutable(arr, comparer);
+            Assert.AreEqual(arr, new List<int>());
+        }
+
+        [Test]
+        public void SelectionSortMutableOneElementTest()
+        {
+            var arr = new List<int>() { 1 };
+            SelectionSort.SelectionSortMutable(arr, comparer);
+            Assert.AreEqual(arr, new List<int>() { 1 });
+        }
+
+        [Test]
+        public void SelectionSortMutableSimpleArrayTest()
+        {
+            var arr = new List<int>() { 3, 2, 5, 1, 6, 8, 9, 4, 7 };
+            SelectionSort.SelectionSortMutable(arr, comparer);
+            Assert.AreEqual(arr, new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        }
+
+        [Test]
+        public void SelectionSortMutableArrayWithDoublesTest()
+        {
+            var arr = new List<int>() { 3, 2, 3, 1, 4, 2, 1, 4 };
+            SelectionSort.SelectionSortMutable(arr, comparer);
+            Assert.AreEqual(arr, new List<int>() { 1, 1, 2, 2, 3, 3, 4, 4 });
+        }
+
+        [Test]
+        public void SelectionSortMutableSomeArraysTest()
+        {
+            for (int s = 1; s < 10; s++)
+            {
+                var arr = Enumerable.Range(1, s).ToList();
+                var tmpArr = arr.Select(item => item).ToList();
+                Shuffle(tmpArr);
+                SelectionSort.SelectionSortMutable(tmpArr, comparer);
+                Assert.AreEqual(tmpArr, arr);
+            }
+        }
+
+
+        private Random rng = new Random();
+        private void Shuffle<T>(IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
 
     }
 }
--- AlgorithmsCSharp/Sort/SelectionSort/SelectionSort.cs
+++ AlgorithmsCSharp/Sort/SelectionSort/SelectionSort.cs
@@ -6,21 +6,43 @@ namespace AlgorithmsCSharp.Sort.SelectionSort
 {
     public class SelectionSort
     {
-        public static List<T> SelectionSortImmutable<T>(List<T> arr, IComparer<T> comparer)
+        public static List<T> SelectionSortImmutableForValueTypes<T>(List<T> arr, IComparer<T> comparer)
         {
             if (arr == null || arr.Count <= 1) { return arr; }
+
+            var tmpArr = arr.Select(item => item).ToList();
 
             var outArr = new List<T>();
             for (int i = 0; i < arr.Count; i++)
             {
-                var minIndex = FindMinElement(arr.TakeLast(arr.Count - 1 - i).ToList(), comparer) + i;
-                outArr.Add(arr[minIndex]);
+                var minIndex = FindMinElement(tmpArr, comparer);
+                outArr.Add(tmpArr[minIndex]);
+                tmpArr.RemoveAt(minIndex);
             }
 
             return outArr;
-
         }
 
+        public static void SelectionSortMutable<T>(List<T> arr, IComparer<T> comparer)
+        {
+            if (arr != null && arr.Count > 0)
+            {                
+                for(int i = 0; i < arr.Count; i++)
+                {
+                    var minIndex = FindMinElement(arr.TakeLast(arr.Count - i).ToList(), comparer) + i;
+                    Swap(arr, minIndex, i);
+                }
+            }
+        }
+
+        private static void Swap<T>(List<T> arr, int firstIndex, int secondIndex)
+        {
+            if(firstIndex >= arr.Count || secondIndex >= arr.Count) { throw new ArgumentException(); }
+            var tmp = arr[firstIndex];
+            arr[firstIndex] = arr[secondIndex];
+            arr[secondIndex] = tmp;
+
+        }
         private static int FindMinElement<T>(List<T> arr, IComparer<T> comparer)
         {
             if (arr == null || arr.Count == 0) { throw new ArgumentException(); }
